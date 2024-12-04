e358ab4a879cd95882cb0ffcfa9a3629111ec1ea
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2024-12-04 13:36:22 +0100

    missing liquibase updates

--- liquibase/changesets/00058-dcat-ap-plugin.sql
+++ liquibase/changesets/00058-dcat-ap-plugin.sql
@@ -3,3 +3,6 @@
 
 -- fix references to dcat2
 update schematron set schemaname = 'dcat-ap' where schemaname = 'dcat2';
+update metadata set schemaid = 'dcat-ap' where schemaid = 'dcat2';
+update metadatadraft set schemaid = 'dcat-ap' where schemaid = 'dcat2';
+update harvestersettings set value = 'schema:dcat-ap:convert/fromSPARQL-DCAT-with-open-keywords' where value = 'schema:dcat2:convert/fromSPARQL-DCAT-with-open-keywords';
