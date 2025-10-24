2449b5687a8f4310cc422dbfdf95ac29ad749490
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2025-10-24 15:19:34 +0200

    fixed current tests and upgraded cypress

--- vlaanderen/cypress-e2e/cypress/e2e/add-new-record.cy.ts
+++ vlaanderen/cypress-e2e/cypress/e2e/add-new-record.cy.ts
@@ -9,7 +9,7 @@ describe('Add new record', () => {
     cy.visit('/srv/eng/catalog.edit#/create')
     // click the 'dataset' item
     cy.get('.col-sm-2 > .panel > .panel-body > .list-group').contains('dataset').click()
-    cy.get('.col-sm-4 > .panel-default > .panel-body > .list-group').contains('Open data (niet-geo)').click()
+    cy.get('.col-sm-4 > .panel-default > .panel-body > .list-group').contains('Generic Open data, conforming to DCAT-AP VL v2.0').click()
     cy.get('[type="button"][data-gn-click-and-spin="createNewMetadata(true)"]').click()
     cy.get('.nav-tabs >> a').contains('Dataset').should('exist')
     cy.get('.nav-tabs >> a').contains('Distributions').should('exist')
--- vlaanderen/cypress-e2e/cypress/e2e/idempotent-editor-save.cy.ts
+++ vlaanderen/cypress-e2e/cypress/e2e/idempotent-editor-save.cy.ts
@@ -1,6 +1,6 @@
 describe('Idempotent editor save', () => {
 
-  const uuid = 'b55d73cc-73b1-441e-a769-5d1b3df9f9a5'
+  const uuid = 'd3a696c7-1a0e-4d27-8774-b39bf98243dd'
 
   beforeEach(() => {
     // first login
@@ -64,7 +64,7 @@ describe('Idempotent editor save', () => {
       cy.get('.gn-md-edit-btn').click()
       cy.wait(1000)
       cy.get('#gn-editor-btn-close').click()
-      cy.get("h1").contains("[test-record] open non-geo").should('exist')
+      cy.get("h1").contains("[cypress] - DCAT-AP-VL Dataset - v2").should('exist')
 
       cy.fixture('dataset-open-nongeo.xml').then(xml => {
         const expected = xml.toString().replace('7c2bab5c-7af1-42da-af4c-921c8615a2cd', uuid).trim()
