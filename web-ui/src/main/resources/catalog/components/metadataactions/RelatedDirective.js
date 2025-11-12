319a790c6504c5a24cb91956e28faafb60d411d4
Joachim Nielandt <joachim.nielandt@vlaanderen.be>
2025-11-12 14:23:58 +0100

    prettify

--- web-ui/src/main/resources/catalog/components/metadataactions/RelatedDirective.js
+++ web-ui/src/main/resources/catalog/components/metadataactions/RelatedDirective.js
@@ -1387,7 +1387,7 @@
           scope.relatedConfigUI = [];
           // this should be improved: sidepanel siblings popup has to have equivalent behaviour to the editor siblings popup
           // however, related configuration is only available in the directive; to check how to centralise this information
-          gnCurrentEdit.relatedConfigUI = scope.relatedConfigUI
+          gnCurrentEdit.relatedConfigUI = scope.relatedConfigUI;
           scope.relatedResourcesConfig = gnRelatedResources;
           if ($injector.has("gnOnlinesrc")) {
             scope.onlinesrcService = $injector.get("gnOnlinesrc");
